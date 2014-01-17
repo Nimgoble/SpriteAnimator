6f177df88e577ae01857f5f939d54b6675abb7e6
Nimgoble <nimgoble@gmail.com>
2014-01-17 15:31:03 -0600

    Added AForge and Blob Selection

---
+++ SpriteAnimator/SpriteAnimator/Controls/AForgeBlobDisplay.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+using AForge.Imaging;
+
+namespace SpriteAnimator.Controls
+{
+    /// <summary>
+    /// Interaction logic for AForgeBlobDisplay.xaml
+    /// </summary>
+    public partial class AForgeBlobDisplay : UserControl
+    {
+        private ImageCanvas imageCanvas = null;
+        public AForgeBlobDisplay()
+        {
+            InitializeComponent();
+            this.itemsControl.SelectionChanged += SelectedItemsChanged;
+        }
+
+        #region Overrides
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            var desiredSize = (imageSource == null) ? base.MeasureOverride(constraint) : new Size(imageSource.PixelWidth + 25, imageSource.PixelHeight + 25);
+            return desiredSize;
+        }
+
+        #endregion
+
+        #region Callbacks
+
+        private void SelectedItemsChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (Blob blob in e.AddedItems)
+            {
+                this.SelectedItems.Add(blob);
+            }
+
+            foreach (Blob blob in e.RemovedItems)
+            {
+                this.SelectedItems.Remove(blob);
+            }
+        }
+
+        private void ImageCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            imageCanvas = sender as ImageCanvas;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void ProcessImage(String imageLocation)
+        {
+            try
+            {
+                ImageSource = new BitmapImage(new Uri(imageLocation));
+                //This one is for AForge
+                System.Drawing.Bitmap bitmap = AForge.Imaging.Image.FromFile(imageLocation);
+                System.Drawing.Bitmap bitmap2 = AForge.Imaging.Image.Clone(bitmap, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                BlobCounter blobCounter = new BlobCounter();
+                blobCounter.ProcessImage(bitmap2);
+
+                items.Clear();
+                Blob[] blobs = blobCounter.GetObjectsInformation();
+                foreach (Blob blob in blobs)
+                {
+                    items.Add(blob);
+                }
+
+                this.InvalidateMeasure();
+            }
+            catch (Exception ex)
+            {
+                String debugMe = String.Empty;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        private ObservableCollection<Blob> items = new ObservableCollection<Blob>();
+        public ObservableCollection<Blob> Items
+        {
+            get { return items; }
+            set
+            {
+                if (value == items)
+                    return;
+
+                items = value;
+            }
+        }
+
+        private ObservableCollection<Blob> selectedItems = new ObservableCollection<Blob>();
+        public ObservableCollection<Blob> SelectedItems
+        {
+            get { return selectedItems; }
+            set
+            {
+                if (value == selectedItems)
+                    return;
+
+                selectedItems = value;
+            }
+        }
+
+        private BitmapImage imageSource = null;
+        public BitmapImage ImageSource
+        {
+            get { return imageSource; }
+            set
+            {
+                if (value == imageSource)
+                    return;
+
+                imageSource = value;
+                if (this.imageCanvas != null)
+                    imageCanvas.ImageSource = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ SpriteAnimator/SpriteAnimator/Controls/ImageCanvas.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpriteAnimator.Controls
+{
+    /// <summary>
+    /// Interaction logic for ImageCanvas.xaml
+    /// </summary>
+    public partial class ImageCanvas : Canvas
+    {
+        public static readonly DependencyProperty _ImageSource = DependencyProperty.Register("ImageSource", typeof(BitmapImage), typeof(ImageCanvas), new FrameworkPropertyMetadata { BindsTwoWayByDefault = true });
+
+        public BitmapImage ImageSource
+        {
+            get { return (BitmapImage)GetValue(_ImageSource); }
+            set
+            {
+                SetValue(_ImageSource, value);
+                imageDrawLocation = new Rect(0, 0, value.PixelWidth, value.PixelHeight);
+                this.InvalidateVisual();
+            }
+        }
+
+        //private BitmapImage sourceImage = null;
+        private Rect imageDrawLocation = new Rect();
+        public ImageCanvas()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            if (ImageSource != null)
+                dc.DrawImage(ImageSource, imageDrawLocation);
+
+            base.OnRender(dc);
+        }
+
+        /*public void SetImage(String imageLocation)
+        {
+            BitmapImage image = new BitmapImage(new Uri(imageLocation));
+            sourceImage = image;
+            imageDrawLocation = new Rect(0, 0, sourceImage.PixelWidth, sourceImage.PixelHeight);
+        }*/
+    }
+}
